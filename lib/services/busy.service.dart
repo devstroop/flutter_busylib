6dd026a742739fa85b3882a037a209e41ffb4dc6
akash <aksbju@gmail.com>
2023-06-13 15:11:37 +0530

    Update

--- lib/services/busy.service.dart
+++ lib/services/busy.service.dart
@@ -1,169 +1,197 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../types/http_method.type.dart';
 
 class BusyService {
   final String baseUrl;
   final String compCode;
-  final String userName;
+  final String username;
   final String password;
 
-  const BusyService({required this.baseUrl, required this.compCode, required this.userName, required this.password});
+  const BusyService(
+      {required this.baseUrl,
+      required this.compCode,
+      required this.username,
+      required this.password});
 
-  Future<http.Response> executeRequest(String endpoint, Map<String, dynamic> parameters) async {
-    final url = Uri.parse(baseUrl + endpoint);
-    final response = await http.post(url, body: parameters);
-    return response;
+  Future openCompany() async {
+    final parameters = {
+      'SC': '51', // Open company
+      'CompCode': compCode,
+      'UserName': username,
+      'Pwd': password,
+    };
+
+    final response = await executeAPIRequest(
+      headers: parameters
+    );
+    return response.body;
+  }
+  Future closeCompany() async {
+    final parameters = {
+      'SC': '52', // Close company
+    };
+    final response = await executeAPIRequest(
+      headers: parameters
+    );
+    return response.body;
   }
 
-  Future<String> getXmlFromRecordset(String query, String username, String password) async {
-    const endpoint = 'get-xml-from-recordset';
+  Future<http.Response> executeAPIRequest({
+    HttpMethod method = HttpMethod.get,
+    String? path,
+    Map<String, String>? headers,
+    Map<String, dynamic>? body,
+    Encoding? encoding,
+  }) async {
+    final url = Uri.parse(baseUrl + (path ?? ''));
+    final response = method == HttpMethod.get
+        ? await http.post(url, headers: headers, body: body, encoding: encoding)
+        : await http.get(url, headers: headers);
+
+    final resultHeader = response.headers['Result'];
+    if (resultHeader == 'F') {
+      final descriptionHeader = response.headers['Description'];
+      throw Exception(descriptionHeader);
+    } else if (resultHeader == 'T') {
+      if (response.statusCode == 200) {
+        return response;
+      } else {
+        throw Exception('Invalid status code in response');
+      }
+    } else {
+      throw Exception('Unspecified \'Result\' in response header');
+    }
+  }
+
+  Future<String> getRecordsetFromXML(String query) async {
     final parameters = {
+      'SC': '1',
+      'UserName': username,
+      'Pwd': password,
       'Qry': query,
-      'UserName': username,
-      'Pwd': password,
     };
 
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to get XML from recordset');
-    }
-  }
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
 
-  Future<String> addVoucherFromXml(String voucherType, String voucherXml, String username, String password) async {
-    const endpoint = 'add-voucher-from-xml';
+  // Voucher
+
+  Future<String> getVchXml(int voucherCode) async {
     final parameters = {
-      'VchType': voucherType,
-      'VchXML': voucherXml,
       'UserName': username,
       'Pwd': password,
+      'VoucherCode': '$voucherCode',
     };
 
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to add voucher from XML');
-    }
-  }
-
-  Future<String> modifyVoucherFromXmlByVoucherNo(String voucherType, String voucherXml, int modifyKey, String username, String password) async {
-    const endpoint = 'modify-voucher-from-xml-by-voucher-no';
-    final parameters = {
-      'VchType': voucherType,
-      'VchXML': voucherXml,
-      'ModifyKey': modifyKey,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to modify voucher from XML');
-    }
-  }
-
-  Future<String> modifyVoucherFromXmlByVoucherCode(String voucherType, String voucherXml, int voucherCode, String username, String password) async {
-    const endpoint = 'modify-voucher-from-xml-by-voucher-code';
-    final parameters = {
-      'VchType': voucherType,
-      'VchXML': voucherXml,
-      'VoucherCode': voucherCode,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to modify voucher from XML');
-    }
-  }
-
-  Future<String> addMasterFromXml(String masterType, String masterXml, String username, String password) async {
-    const endpoint = 'add-master-from-xml';
-    final parameters = {
-      'MasterType': masterType,
-      'MasterXML': masterXml,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to add master from XML');
-    }
-  }
-
-  Future<String> modifyMasterFromXmlByCode(String masterCode, String masterXml, String username, String password) async {
-    const endpoint = 'modify-master-from-xml-by-code';
-    final parameters = {
-      'MasterCode': masterCode,
-      'MasterXML': masterXml,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to modify master from XML');
-    }
-  }
-
-  Future<String> modifyMasterFromXmlByName(String masterName, String masterType, String masterXml, String username, String password) async {
-    const endpoint = 'modify-master-from-xml-by-name';
-    final parameters = {
-      'MasterName': masterName,
-      'MasterType': masterType,
-      'MasterXML': masterXml,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to modify master from XML');
-    }
-  }
-
-  Future<String> getVchXml(int voucherCode, String username, String password) async {
-    const endpoint = 'get-vch-xml';
-    final parameters = {
-      'VoucherCode': voucherCode,
-      'UserName': username,
-      'Pwd': password,
-    };
-
-    final response = await executeRequest(endpoint, parameters);
+    final response = await executeAPIRequest(headers: parameters);
     if (response.statusCode == 200) {
       return response.body;
     } else {
       throw Exception('Failed to get voucher XML');
     }
-  }
+  } // Non working
 
-  Future<String> getMasterXml(int masterCode, String username, String password) async {
-    const endpoint = 'get-master-xml';
+  Future<String> addVoucherFromXml(
+      String voucherType, String voucherXml) async {
     final parameters = {
-      'MasterCode': masterCode,
+      'SC': '2',
       'UserName': username,
       'Pwd': password,
+      'VchType': voucherType,
+      'VchXML': voucherXml,
     };
 
-    final response = await executeRequest(endpoint, parameters);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to get master XML');
-    }
-  }
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
+  Future<String> modifyVoucherFromXmlByKey(
+      String voucherType, String voucherXml, int modifyKey) async {
+    Map<String, String> parameters = {
+      'SC': '3', // Modify Vch By Key
+      'UserName': username,
+      'Pwd': password,
+      'VchType': voucherType,
+      'VchXML': voucherXml,
+      'ModifyKey': '$modifyKey',
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
+  Future<String> modifyVoucherFromXmlByVoucherCode(
+      String voucherType, String voucherXml, int voucherCode) async {
+    Map<String, String> parameters = {
+      'SC': '4', // Modify Vch By VchCode
+      'UserName': username,
+      'Pwd': password,
+      'VchType': voucherType,
+      'VchXML': voucherXml,
+      'VoucherCode': '$voucherCode',
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
+
+  // Master
+  Future<String> getMasterXml(int masterCode) async {
+    final parameters = {
+      'UserName': username,
+      'Pwd': password,
+      'MasterCode': '$masterCode',
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // Non working
+
+  Future<String> addMasterFromXml(String masterType, String masterXml) async {
+    final parameters = {
+      'SC': '5',
+      'UserName': username,
+      'Pwd': password,
+      'MasterType': masterType,
+      'MasterXML': masterXml,
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
+  Future<String> modifyMasterFromXmlByCode(
+      String masterCode, String masterXml) async {
+    final parameters = {
+      'SC': '6', // Modify master by Code
+      'UserName': username,
+      'Pwd': password,
+      'MasterCode': masterCode,
+      'MasterXML': masterXml,
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
+  Future<String> modifyMasterFromXmlByName(
+      String masterName, String masterType, String masterXml) async {
+    final parameters = {
+      'SC': '7', // Modify a master through XML string (by its Name)
+      'UserName': username,
+      'Pwd': password,
+      'MasterName': masterName,
+      'MasterType': masterType,
+      'MasterXML': masterXml,
+    };
+
+    final response = await executeAPIRequest(headers: parameters);
+    return response.body;
+  } // OK
+
 }
---
+++ lib/types/http_method.type.dart
@@ -0,0 +1,4 @@
+enum HttpMethod{
+  get,
+  post
+}
