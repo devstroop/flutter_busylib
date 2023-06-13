cfcfd57949960b3b4f946df7e4c0bff589e9e11f
akash <aksbju@gmail.com>
2023-06-13 16:06:02 +0530

    Update

--- lib/providers/common_db/company.provider.dart
+++ lib/providers/common_db/company.provider.dart
@@ -1,23 +1,14 @@
-
-import 'package:flutter/material.dart';
-
 import '../../models/common_db/company.model.dart';
 import '../../services/busy.service.dart';
 import '../../utils/xml.util.dart';
 
-class CompanyProvider with ChangeNotifier {
-  Company? _company;
-  final BusyService _busyService;
-
-  CompanyProvider(this._busyService);
-
-  Company? get company => _company;
-
-
+class CompanyProvider {
+  final BusyService service;
+  CompanyProvider(this.service);
   Future<Company> get() async {
     try {
-      final xml = await _busyService.getRecordsetFromXML('SELECT * FROM Company');
-      final parsedData = parseXml(xml); // Implement XML parsing logic here
+      final xml = await service.getRecordsetFromXML('SELECT * FROM Company');
+      final parsedData = parseXml(xml);
       return Company.fromXml(parsedData);
     } catch (error) {
       throw Exception('Failed to fetch company details: $error');
