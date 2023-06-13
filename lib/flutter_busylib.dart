ab3c559bb3840c255cf73af9bdbb1a8a369fb0f0
akash <aksbju@gmail.com>
2023-06-13 16:25:04 +0530

    Update

--- lib/flutter_busylib.dart
+++ lib/flutter_busylib.dart
@@ -1,24 +1,22 @@
-library reetail_helper;
+library flutter_busylib;
 
-import 'package:flutter/material.dart';
-import 'package:reetail_helper/providers/common_db/company.provider.dart';
-import 'package:reetail_helper/providers/common_db/printer_reps.provider.dart';
-import 'package:reetail_helper/providers/common_db/screen_reps.provider.dart';
-import 'package:reetail_helper/providers/common_db/std_nar.provider.dart';
-import 'package:reetail_helper/providers/common_db/user_log.provider.dart';
-import 'package:reetail_helper/providers/common_db/user_preference.provider.dart';
-import 'package:reetail_helper/providers/common_db/user_preference1.provider.dart';
-import 'package:reetail_helper/services/busy.service.dart';
+import 'package:flutter_busylib/providers/common_db/company.provider.dart';
+import 'package:flutter_busylib/providers/common_db/printer_reps.provider.dart';
+import 'package:flutter_busylib/providers/common_db/screen_reps.provider.dart';
+import 'package:flutter_busylib/providers/common_db/std_nar.provider.dart';
+import 'package:flutter_busylib/providers/common_db/user_log.provider.dart';
+import 'package:flutter_busylib/providers/common_db/user_preference.provider.dart';
+import 'package:flutter_busylib/providers/common_db/user_preference1.provider.dart';
+import 'package:flutter_busylib/services/busy.service.dart';
 
-
-class ReetailHelper with ChangeNotifier{
+class BusyLib{
   final String baseUrl;
   final String compCode;
   final String username;
   final String password;
 
   BusyService? busyService;
-  ReetailHelper({required this.baseUrl, required this.compCode, required this.username, required this.password});
+  BusyLib({required this.baseUrl, required this.compCode, required this.username, required this.password});
   openDB(){
     busyService = BusyService(baseUrl: baseUrl, compCode: compCode, username: username, password: password);
   }
@@ -30,7 +28,6 @@ class ReetailHelper with ChangeNotifier{
   CommonDB? get commonDB => busyService != null ? CommonDB(busyService!) : null;
   YearSpecificDB? get yearSpecificDB => busyService != null ? YearSpecificDB(busyService!) : null;
 }
-
 class CommonDB{
   final BusyService service;
   CommonDB(this.service);
