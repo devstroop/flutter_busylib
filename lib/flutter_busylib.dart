88595b66db30b23198582eaecefce0b8089655fb
akash <aksbju@gmail.com>
2023-06-15 05:42:32 +0530

    Update

--- lib/flutter_busylib.dart
+++ lib/flutter_busylib.dart
@@ -4,26 +4,21 @@ import 'package:flutter_busylib/providers/master_entity/folio1.provider.dart';
 import 'package:flutter_busylib/providers/master_entity/master1.provider.dart';
 import 'package:flutter_busylib/services/busy.service.dart';
 
-import 'models/master_entity/master1.dart';
-
 class BusyLib{
   final String _baseUrl;
   final String _compCode;
   final String _username;
   final String _password;
 
-  late BusyService? _busyService;
+  BusyService? _busyService;
   
-  BusyLib({required String baseUrl, required String compCode, required String username, required String password}) : _password = password, _username = username, _compCode = compCode, _baseUrl = baseUrl;
-  
-  openDB(){
+  BusyLib(this._baseUrl, this._compCode, this._username, this._password){
     _busyService = BusyService(baseUrl: _baseUrl, compCode: _compCode, username: _username, password: _password);
-    _busyService?.openCompany();
   }
-  closeDB(){
-    _busyService?.closeCompany();
-    _busyService = null;
-  }
+  
+  Future? open() => _busyService?.openCompany();
+  Future? close() => _busyService?.closeCompany();
+  Future? dispose() => _busyService = null;
 
   Folio1Provider? get folio1Provider => _busyService != null ? Folio1Provider(_busyService!) : null;
   Master1Provider? get master1Provider => _busyService != null ? Master1Provider(_busyService!) : null;
